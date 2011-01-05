f1acb992fb74b5015cf2a1d01c9c994c5d2c50df
Choon Hong Peck <chpeck@thingbuzz.com>
2011-01-05 15:54:16 -0800

    only load buzz.js if it is not already on the page.

--- buzzgrowl.js
+++ buzzgrowl.js
@@ -1,13 +1,27 @@
+var script_url = 'http://thingbuzz.com/embed/buzz.js';
+var load = true;
+
 chrome.extension.sendRequest({state: "current"}, function(response) {
-  if (response.state) {
+  var scripts = document.getElementsByTagName("script");
+  for(i in scripts) {
+    if (scripts[i].src == script_url) {
+      load = false;
+      break;
+    }
+  }
+  
+  if (response.state && load) {
+    console.log('loading script');
     var head = document.getElementsByTagName('head')[0];
     function growl() {
       var growl = document.createElement('script');
       growl.innerHTML = 'new TBZZ.Growl()';
       head.appendChild(growl);
     }
+    
+    
     var buzz = document.createElement('script');
-    buzz.src = 'http://thingbuzz.com/embed/buzz.js';
+    buzz.src = script_url;
     buzz.onload = growl;
     head.appendChild(buzz);    
   }
