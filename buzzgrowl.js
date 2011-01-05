08169ab836dd2fafa905800ec35e675a43abd831
Choon Hong Peck <chpeck@thingbuzz.com>
2011-01-05 14:43:03 -0800

    Switch to using onload to detect when to create the growl.

--- buzzgrowl.js
+++ buzzgrowl.js
@@ -1,7 +1,10 @@
 var head = document.getElementsByTagName('head')[0];
+function growl() {
+  var growl = document.createElement('script');
+  growl.innerHTML = 'new TBZZ.Growl()';
+  head.appendChild(growl);
+}
 var buzz = document.createElement('script');
 buzz.src = 'http://thingbuzz.com/embed/buzz.js';
+buzz.onload = growl;
 head.appendChild(buzz);
-var growl = document.createElement('script');
-growl.innerHTML = 'new TBZZ.Growl()';
-head.appendChild(growl);
